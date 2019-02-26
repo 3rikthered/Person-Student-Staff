95643c73cbdaf8a1aba7c0c45e9665dafb3d4e02
Erik <3rikthered.main@gmail.com>
2019-02-26 15:27:08 -0500

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -1,20 +1,18 @@
 package nelsonTask1;
 
 public class Faculty extends Employee{
-	
 	private String officeHours;
 	private int rank;
 	
-	public Faculty()
+	public Faculty(String name,  String address, String phoneNumber,
+	        String emailAddress, String office, double salary,
+	        MyDate dateHired, String officeHours, int rank)
 	{
-	}
-	
-	public Faculty(String officeHours, int rank)
-	{
+		super(name, address, phoneNumber, emailAddress, office, salary, dateHired);
+		
 		this.officeHours = officeHours;
 		this.rank = rank;
 	}
-
 	
 	// GETTERS
 	public String getOfficeHours()
@@ -26,7 +24,6 @@ public class Faculty extends Employee{
 	{
 		return rank;
 	}
-
 	
 	// SETTERS
 	public void setOfficeHours(String officeHours)
@@ -38,7 +35,4 @@ public class Faculty extends Employee{
 	{
 		this.rank = rank;
 	}
-	
-	
-
 }
